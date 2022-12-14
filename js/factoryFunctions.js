3fae927eae2f22555ad1ed33960ba4b13d03abdf
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-12-14 12:06:44 +0900

    push

--- js/factoryFunctions.js
+++ js/factoryFunctions.js
@@ -1,9 +1,15 @@
-const elf = {
-  name: "Orwell",
-  weapon: "bow",
-  attack() {
-    return "attack with " + this.weapon;
-  },
+//Constructor functions
+//加this
+//加大寫
+function Elf(name, weapon) {
+  console.log("this", this);
+  this.name = name;
+  this.weapon = weapon;
+}
+
+Elf.prototype.attack = function () {
+  return `attack with ${this.weapon}`;
 };
 
-console.log(elf.attack());
+const peter = new Elf("peter", "stone"); //一定要做new
+console.log(peter.attack());
